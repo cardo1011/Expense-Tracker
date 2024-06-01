5d6d31ebc55e6fba1d168d88ab504a5a39b17ced
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-05-31 23:46:33 -0700

    add functionality to "reset" button
    "reset" button clears input fields when clicked.

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const addBtn = document.getElementById("add");
 const resetBtn = document.getElementById("reset");
 const expenseTable = document.getElementById("expense-table");
 
+// clears input fields when "reset" button is clicked
 const resetInputs = function () {
-  expenseType.innerTe.value = "";
-  date.innerHTML.value = innerHTML.value = "";
-  dollarAmt.innerText.value = innerHTML.value = "";
+  expenseType.value = "";
+  date.value = "";
+  dollarAmt.value = "";
 
   expenseTable.style.display = "none";
 };
