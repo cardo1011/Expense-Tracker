b0437c99a07dd9deb8778b4223a038ef84006795
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-06-02 18:18:19 -0700

    implementing addExpense fn
    trying to figure out how to only allow two decimal places in dollarAmt input

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const dollarAmt = document.getElementById("dollar-amount");
 const addBtn = document.getElementById("add");
 const resetBtn = document.getElementById("reset");
 const expenseTable = document.getElementById("expense-table");
+let expense = [];
+let expenseID = 0;
 
 // clears input fields when "reset" button is clicked
 const resetInputs = function () {
@@ -18,4 +20,23 @@ const resetInputs = function () {
 
 resetBtn.addEventListener("click", function () {
   resetInputs();
+});
+
+const addExpense = function () {
+  if (!expenseType.value || !date.value || !dollarAmt.value) {
+    alert("Finish inputting the corresponding information for the expense.");
+  }
+
+  if (expenseType.value && date.value && dollarAmt.value) {
+    expense = {
+      id: expenseID,
+      type: expenseType.value.trim(),
+      date: date.value,
+      amount: dollarAmt.value.
+    };
+  }
+};
+
+addBtn.addEventListener("click", function () {
+  addExpense();
 });
