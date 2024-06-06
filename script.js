42624684069a0914b0eef9262058ac0ad6ee8ba5
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-06-06 15:39:42 -0700

    removeBtn added to each row when rendered
    removeBtn added to each row when rendered. CSS added to button.
    
    Final version of project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//add a button to each row after it renders to remove the expense from the array and the table
+
 const expenseType = document.getElementById("expense-type");
 const date = document.getElementById("date");
 const dollarAmt = document.getElementById("dollar-amount");
@@ -9,13 +11,14 @@ const expenseTable = document.getElementById("expense-table");
 let expensesArray = [];
 let ID = 0;
 
-// clears input fields when "reset" button is clicked
+/*starting without displaying the table*/
+expenseTable.style.display = "none";
+
+/*clears input fields when "reset" button clicked*/
 const resetInputs = function () {
   expenseType.value = "";
   date.value = "";
   dollarAmt.value = "";
-
-  expenseTable.style.display = "none";
 };
 
 resetBtn.addEventListener("click", function () {
@@ -23,11 +26,13 @@ resetBtn.addEventListener("click", function () {
 });
 
 const addExpense = function () {
+  /*making sure all inputs are filled fully, else an alert will appear*/
   if (!expenseType.value || !date.value || !dollarAmt.value) {
     alert("Finish inputting the corresponding information for the expense.");
   }
 
   if (expenseType.value && date.value && dollarAmt.value) {
+    /*creating an object with a unique id*/
     let expense = {
       id: ID++,
       type: expenseType.value.trim(),
@@ -35,10 +40,11 @@ const addExpense = function () {
       amount: dollarAmt.value,
     };
 
+    /*adding the expense object to the expensesArray*/
     expensesArray.push(expense);
-
-    console.log(expensesArray);
   }
+
+  /*rendering the objects to the table*/
   renderRow(expensesArray);
 };
 
@@ -47,23 +53,46 @@ addBtn.addEventListener("click", function () {
 });
 
 const renderRow = function (array) {
+  /*Selecting the table */
   let myTable = document
     .getElementById("expense-table")
     .getElementsByTagName("tbody")[0];
 
+  /*clearing the table before rendering the elements of the array in the table*/
   myTable.innerHTML = "";
 
   for (let i = 0; i < array.length; i++) {
-    let row = myTable.insertRow(i);
-    let expenseTypeCell = row.insertCell(0);
-    let dateCell = row.insertCell(1);
-    let amountCell = row.insertCell(2);
+    /*iterating through the array of objects*/
+    let row =
+      myTable.insertRow(
+        i
+      ); /*indicating where the row should be inserted into the table*/
 
+    let expenseTypeCell = row.insertCell(0); /*inserting cell for expenseType*/
+    let dateCell = row.insertCell(1); /*inserting cell for date*/
+    let amountCell = row.insertCell(2); /*inserting cell for dollarAmt*/
+    let btnCell = row.insertCell(3); /*inserting cell for removeBtn*/
+
+    /*declaring what data each cell will display*/
     expenseTypeCell.innerHTML = array[i].type;
     dateCell.innerHTML = array[i].date;
     amountCell.innerHTML = array[i].amount;
+
+    /*creating a remove button for the specific cell*/
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.className = "remove-btn";
+    /*event listener added to the remove-btn*/
+    removeBtn.addEventListener("click", function () {
+      array.splice(i, 1); /*removes the corresponding row from the array*/
+
+      renderRow(array); /*render array again to update the UI*/
+    });
+
+    btnCell.appendChild(removeBtn); /*adding btn to cell*/
   }
 
+  /*making table display if length of array is greater than 0*/
   if (array.length > 0) {
     expenseTable.style.display = "";
   }
